1139e1a195be62b2d9f5ae920e30e364cb9d411e
MOHIT LAXKAR <laxkarmohit25@gmail.com>
2021-06-11 20:40:51 +0530

    files updated

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 import Rehabilitation from './Rehabilitation';
 import Mission from './Mission';
 import Journey from './Journey';
+import ImageSlider from '../Home/HomeCarousel/ImageSlider';
+import {SliderData} from '../Home/HomeCarousel/SliderData';
+import Collaboration from './HomeCollaboration/Collaboration';
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <ImageSlider slides = {SliderData}/>
                 <Rehabilitation />
                 <Mission />
                 <Journey />
+                <Collaboration/>
             </div>
         );
     }
---
+++ client/src/Components/Home/HomeCarousel/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+    {
+        image: "./images/101.png"
+    },
+    {
+        image: "./images/102.png"
+    },
+    {
+        image: "./images/103.png"
+    },
+    {
+        image: "./images/104.png"
+    },
+    {
+        image: "./images/105.png"
+    },
+    {
+        image: "./images/106.png"
+    },
+    {
+        image: "./images/107.png"
+    }
+]
