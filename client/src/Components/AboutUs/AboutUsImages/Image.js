1f90b49ed20ded3a6bd39e46a7367e58a0ecec0f
shataxi <dubey.shataxi3010@gmail.com>
2021-06-17 20:41:17 +0530

    image url changed

--- client/src/Components/AboutUs/AboutUsImages/Image.js
+++ client/src/Components/AboutUs/AboutUsImages/Image.js
@@ -1,33 +1,61 @@
 const Image = {
-    /* Section1: require("./AboutUsImages/Section1.jpeg").default, */
-    Section1: require("../AboutUsImages/Section1.jpeg").default,
-    Section3: require("../AboutUsImages/Section3.jpeg").default,
-    Section5: require("../AboutUsImages/Section5.jpeg").default,
-    Section6: {
-      Img1: require("../AboutUsImages/Section6_1.jpeg").default,
-      Img2: require("../AboutUsImages/Section6_2.jpeg").default,
-      Img3: require("../AboutUsImages/Section6_3.jpeg").default,
-    },
-    Section7: {
-      Img1: require("../AboutUsImages/Section7_1.jpeg").default,
-      Img2: require("../AboutUsImages/Section7_2.jpeg").default,
-      Img3: require("../AboutUsImages/Section7_3.jpeg").default,
-      Img4: require("../AboutUsImages/Section7_4.jpeg").default,
-    },
-    Section7b: {
-      Img1: require("../AboutUsImages/Section7b_1.png").default,
-      Img2: require("../AboutUsImages/Section7b_2.png").default,
-      Img3: require("../AboutUsImages/Section7b_3.png").default,
-      Img4: require("../AboutUsImages/Section7b_4.png").default,
-    },
-    Section8: require("../AboutUsImages/Section8.jpeg").default,
-    Section9: {
-      Img1: require("../AboutUsImages/Section9_1.jpeg").default,
-      Img2: require("../AboutUsImages/Section9_2.jpeg").default,
-      Img3: require("../AboutUsImages/Section9_3.jpeg").default,
-      Img4: require("../AboutUsImages/Section9_4.jpeg").default,
-      Img5: require("../AboutUsImages/Section9_5.jpeg").default,
-    }
-}
+  /* Section1: require("./AboutUsImages/Section1.jpeg").default, */
+  // Section1: require("../AboutUsImages/Section1.jpeg").default,
+  // Section3: require("../AboutUsImages/Section3.jpeg").default,
+  // Section5: require("../AboutUsImages/Section5.jpeg").default,
+  // Section6: {
+  //   Img1: require("../AboutUsImages/Section6_1.jpeg").default,
+  //   Img2: require("../AboutUsImages/Section6_2.jpeg").default,
+  //   Img3: require("../AboutUsImages/Section6_3.jpeg").default,
+  // },
+  // Section7: {
+  //   Img1: require("../AboutUsImages/Section7_1.jpeg").default,
+  //   Img2: require("../AboutUsImages/Section7_2.jpeg").default,
+  //   Img3: require("../AboutUsImages/Section7_3.jpeg").default,
+  //   Img4: require("../AboutUsImages/Section7_4.jpeg").default,
+  // },
+  // Section7b: {
+  //   Img1: require("../AboutUsImages/Section7b_1.png").default,
+  //   Img2: require("../AboutUsImages/Section7b_2.png").default,
+  //   Img3: require("../AboutUsImages/Section7b_3.png").default,
+  //   Img4: require("../AboutUsImages/Section7b_4.png").default,
+  // },
+  // Section8: require("../AboutUsImages/Section8.jpeg").default,
+  // Section9: {
+  //   Img1: require("../AboutUsImages/Section9_1.jpeg").default,
+  //   Img2: require("../AboutUsImages/Section9_2.jpeg").default,
+  //   Img3: require("../AboutUsImages/Section9_3.jpeg").default,
+  //   Img4: require("../AboutUsImages/Section9_4.jpeg").default,
+  //   Img5: require("../AboutUsImages/Section9_5.jpeg").default,
+  // },
+  Section1: "https://drive.google.com/uc?id=1Ixb8UTLWhOKjPdEEH7cibfwElh8Gs9Jm",
+  Section3: "https://drive.google.com/uc?id=15PKwBt7AIZu0hYjmZECepeYqFTxerb9a",
+  Section5: "https://drive.google.com/uc?id=12zmq8aiALzMlEb1QrQuRmzNVXTMQZWc-",
+  Section6: {
+    Img1: "https://drive.google.com/uc?id=1N_ln_PzP5UiNeek84L6wLm7FrjIQ0FTj",
+    Img2: "https://drive.google.com/uc?id=1WOtI0n-74ygfYEdSHnYh0P85qGtGgBP5",
+    Img3: "https://drive.google.com/uc?id=10j0E88g-LtP9qxi4ClKZ8nsEPkCmK5xC",
+  },
+  Section7: {
+    Img1: "https://drive.google.com/uc?id=1YKXDoO_4kMoCppsO-fw0z7QKm2O36mHn",
+    Img2: "https://drive.google.com/uc?id=1mBYHsiT4ikGKUzk2Hjz_ZKhvyYKh4dfC",
+    Img3: "https://drive.google.com/uc?id=1tc6DjdDpmBaKn7Tp1H9Ua_yxBJdukyw5",
+    Img4: "https://drive.google.com/uc?id=1ZtrHJIAJDrGBXUrVxKJDtQ1nU-mY7jxQ",
+  },
+  Section7b: {
+    Img1: "https://drive.google.com/uc?id=1w4u7Uj0_gItghRdkhDKTf6w9lA2podr9",
+    Img2: "https://drive.google.com/uc?id=1mfQSRTHKtai5jBLe7PMOBDHOauOBvdft",
+    Img3: "https://drive.google.com/uc?id=1ag96TlP0mLKRS6DWecQYJnzNzYRZvQgZ",
+    Img4: "https://drive.google.com/uc?id=10Y87f0sGCJ191JiXdR38q6l8wvQMcJCw",
+  },
+  Section8: "https://drive.google.com/uc?id=1vxaQVQ7nuH5GI_QR_1Fa_a8VL8nmQyF2",
+  Section9: {
+    Img1: "https://drive.google.com/uc?id=1hlRMlkkZ9ZhEoaZINQwONl6BP4_HV2d_",
+    Img2: "https://drive.google.com/uc?id=1cnxZo83kCJ9-OX8fC8ZSXibP15sTZeD4",
+    Img3: "https://drive.google.com/uc?id=1aaq-iGSROj2rMhwBpYto07UFyxL-_uph",
+    Img4: "https://drive.google.com/uc?id=1AiICk0iQBnk1_RfGBlU0ngf00uRyEedM",
+    Img5: "https://drive.google.com/uc?id=1RPk1xx5frMty-QM_ew3qSclJyeEpaeLy",
+  },
+};
 
 export default Image;
