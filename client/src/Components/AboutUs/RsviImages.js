06267b449913a7ac101b14fe44c038fd4c2d78fb
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2021-06-18 18:36:36 +0530

    sec5 and 6

--- client/src/Components/AboutUs/RsviImages.js
+++ client/src/Components/AboutUs/RsviImages.js
@@ -7,7 +7,10 @@ class RsviImages extends Component {
     render() {
         return (
             <div>
-                <Row className={Modules.section5}>
+            <Row className={Modules.sec5bgimage}>
+              <img src={Image.Section5} alt="Sec5" />
+            </Row>
+            <Row className={Modules.section5}>
                   <p className={Modules.para1}>
                     Besides there are other issues to be addressed, such as
                     availability of Aids & Appliances (Assistive Devices) for the
@@ -26,34 +29,32 @@ class RsviImages extends Component {
                     Impairment.
                   </p>
                 </Row>
+
                 {/* **************** Row 6 **************** */}
-                <Row className={Modules.sec5bgimage}>
-                  <img src={Image.Section5} alt="dummy" />
-                </Row>
                 <Row className={Modules.section6}>
                   <Col>
                     <img
                       src={Image.Section6.Img1}
                       alt="dummy"
-                      className={Modules.boxbg1}
+                      className={Modules.sec6img}
                     />
-                    <p className={Modules.sbox}> About Us </p>
+                    <p className={Modules.sec6box}> About Us </p>
                   </Col>
                   <Col>
                     <img
                       src={Image.Section6.Img2}
                       alt="dummy"
-                      className={Modules.boxbg2}
+                      className={Modules.sec6img}
                     />
-                    <p className={Modules.sbox}> About Us </p>
+                    <p className={Modules.sec6box}> About Us </p>
                   </Col>
                   <Col>
                     <img
                       src={Image.Section6.Img3}
                       alt="dummy"
-                      className={Modules.boxbg3}
+                      className={Modules.sec6img}
                     />
-                    <p className={Modules.sbox}> About Us </p>
+                    <p className={Modules.sec6box}> About Us </p>
                   </Col>
                 </Row>
             </div>
