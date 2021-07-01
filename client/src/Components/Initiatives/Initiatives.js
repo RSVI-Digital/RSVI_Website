ceccd9b854b8e0ccc5bb3a3d627d51b3a33bcbcb
Anjali Chaturvedi <anjalichaturvedii@icloud.com>
2021-07-02 00:56:26 +0530

    updated section2

--- client/src/Components/Initiatives/Initiatives.js
+++ client/src/Components/Initiatives/Initiatives.js
@@ -17,7 +17,7 @@ class Initiatives extends Component {
                 <Container className={Modules.containerInitiatives}>
                     <Section1></Section1>
                     <Section2></Section2>
-                    <Section2M></Section2M>
+                    {/*<Section2M></Section2M>*/}
                     <Section3></Section3>
                     <Section4></Section4>
                     <Section5></Section5>
--- client/src/Components/Initiatives/Section2.js
+++ client/src/Components/Initiatives/Section2.js
@@ -17,6 +17,30 @@ const Section2 = () => {
       document.getElementById('containerCard3').style.display = "none";
       document.getElementById('containerCard4').style.display = "block";
     }
+    if (e.target.id === 'Mnext-btn_1') {
+      document.getElementById('containerCardM1').style.display = "none";
+      document.getElementById('containerCardM2').style.display = "block";
+    }
+    if (e.target.id === 'Mnext-btn_2') {
+      document.getElementById('containerCardM2').style.display = "none";
+      document.getElementById('containerCardM3').style.display = "block";
+    }
+    if (e.target.id == 'Mnext-btn_3') {
+      document.getElementById('containerCardM3').style.display = "none";
+      document.getElementById('containerCardM4').style.display = "block";
+    }
+    if (e.target.id == 'Mnext-btn_4') {
+      document.getElementById('containerCardM4').style.display = "none";
+      document.getElementById('containerCardM5').style.display = "block";
+    }
+    if (e.target.id == 'Mnext-btn_5') {
+      document.getElementById('containerCardM5').style.display = "none";
+      document.getElementById('containerCardM6').style.display = "block";
+    }
+    if (e.target.id == 'Mnext-btn_6') {
+      document.getElementById('containerCardM6').style.display = "none";
+      document.getElementById('containerCardM7').style.display = "block";
+    }
   }
 
   const previousCard = (e) => {
@@ -35,15 +59,39 @@ const Section2 = () => {
       document.getElementById('containerCard3').style.display = "block";
       // document.getElementById('containerCard1').style.transition = "all 1200ms ease-in";
   }
+  if (e.target.id == 'Mprev-btn_2') {
+    document.getElementById('containerCardM2').style.display = "none";
+    document.getElementById('containerCardM1').style.display = "block";
+  }
+  if (e.target.id == 'Mprev-btn_3') {
+    document.getElementById('containerCardM3').style.display = "none";
+    document.getElementById('containerCardM2').style.display = "block";
+  }
+  if (e.target.id == 'Mprev-btn_4') {
+    document.getElementById('containerCardM4').style.display = "none";
+    document.getElementById('containerCardM3').style.display = "block";
+  }
+  if (e.target.id == 'Mprev-btn_5') {
+    document.getElementById('containerCardM5').style.display = "none";
+    document.getElementById('containerCardM4').style.display = "block";
+  }
+  if (e.target.id == 'Mprev-btn_6') {
+    document.getElementById('containerCardM6').style.display = "none";
+    document.getElementById('containerCardM5').style.display = "block";
+  }
+  if (e.target.id == 'Mprev-btn_7') {
+    document.getElementById('containerCardM7').style.display = "none";
+    document.getElementById('containerCardM6').style.display = "block";
+  }
 }
 
   return (
-    <div> 
+    <div>
         <h1 className={style.header}>Training Programs</h1>
         <Container className={style.mainTraining}>
         <div className={style.container1} id="containerCard1">
            <Row className={style.Row1}>
-             <Col className={style.containerimage}>
+             <Col className={style.topimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
              <Col className={style.containertext}>
@@ -52,20 +100,20 @@ const Section2 = () => {
              </Col>
            </Row>
            <Row className={style.Row2}>
-             <Col className={style.containertext1}>
+             <Col className={style.containertext}>
               <h2 className={style.containerH2}> Mobility </h2>
                <p> Orientation & Mobility Training is to orient with the surroundings and to teach how to become mobile independently with the use of white cane. </p>
                <input type="button" className={style.previous} value="" />
                <input type="button" className={style.next} value="Next" id="next-btn_1" onClick={nextCard} />
              </Col>
-             <Col className={style.containerimage1}>
+             <Col className={style.bottomimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
            </Row>
         </div>
         <div className={style.container2} id="containerCard2">
-           <Row className={style.Row1}>
-             <Col className={style.containerimage}>
+           <Row className={style.Row3}>
+             <Col className={style.topimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
              <Col className={style.containertext}>
@@ -73,21 +121,21 @@ const Section2 = () => {
                <p> Computer and Laptop Training is imparted at RSVI to the visually Impaired so that they can study and qualify various Competitive Examinations, qualify jobs and further impart training to other Visually Impaired students. </p>
              </Col>
            </Row>
-           <Row className={style.Row2}>
-             <Col className={style.containertext1}>
+           <Row className={style.Row4}>
+             <Col className={style.containertext}>
               <h2 className={style.containerH2}> Mobile Training </h2>
                <p> Mobile Training is imparted at RSVI to the visually Impaired so that they can study and communicate through smart phones , further impart training to other Visually Impaired students.  </p>
                <input type="button" className={style.previous} value="Previous" id="prev-btn_2" onClick={previousCard} />
                <input type="button" className={style.next} value="Next" id="next-btn_2" onClick={nextCard} />
              </Col>
-             <Col className={style.containerimage1}>
+             <Col className={style.bottomimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
            </Row>
         </div>
         <div className={style.container3} id="containerCard3">
            <Row className={style.Row1}>
-             <Col className={style.containerimage}>
+             <Col className={style.topimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
              <Col className={style.containertext}>
@@ -96,25 +144,25 @@ const Section2 = () => {
              </Col>
            </Row>
            <Row className={style.Row2}>
-             <Col className={style.containertext1}>
+             <Col className={style.containertext}>
               <h2 className={style.containerH2}> Vocational Training </h2>
                <p> Vocational Training is imparted at RSVI to the visually Impaired so that they can study and qualify various Competitive Examinations, qualify jobs and further impart training to other Visually Impaired students. </p>
                <input type="button" className={style.previous} value="Previous" id="prev-btn_3" onClick={previousCard} />
                <input type="button" className={style.next} value="Next" id="next-btn_3" onClick={nextCard} />
              </Col>
-             <Col className={style.containerimage1}>
+             <Col className={style.bottomimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
            </Row>
         </div>
         <div className={style.container4} id="containerCard4">
-           <Row className={style.Row1}>
-             <Col className={style.containerimage}>
+           <Row className={style.Row3}>
+             <Col className={style.topimage}>
              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
              </Col>
              <Col className={style.containertext}>
                <h2 className={style.containerH2}> Misc Workshop and Seminar </h2>
-               <p>Misc workshops and Seminars is imparted at RSVI to the visually Impaired so that they can aware Visually Impaired Persons about technology and new updations, latest technology and trends.s </p>
+               <p>Misc workshops and Seminars is imparted at RSVI to the visually Impaired so that they can aware Visually Impaired Persons about technology and new updations, latest technology and trends. </p>
                <input type="button" className={style.previous} value="Previous" id="prev-btn_4" onClick={previousCard} />
                <input type="button" className={style.next} value="" />
              </Col>
@@ -123,8 +171,155 @@ const Section2 = () => {
            </Row>
         </div>
     </Container>
-    </div>  
-    
+    <Container className={style.mainContainer}>
+      <section className={style.container1} id="containerCardM1">
+        <Row className={style.front1} id="front1">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Braille Training</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p>
+              Braille Training is imparted at RSVI to the visually Impaired so that they can study and qualify various Competetive Examinations, qualify jobs and further impart training to other Visually Impaired students.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="" />
+            <input type="button" className={style.next} value="Next" id="Mnext-btn_1" onClick={nextCard} />
+          </Row>
+        </Row>
+      </section>
+
+      <section className={style.container2} id="containerCardM2">
+        <Row className={style.front2} id="front2">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Mobility</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p>
+              Orientation & MobilityTraining is to orient with the surroundings and to teach how to become mobile independently with the use of white cane.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="Previous" id="Mprev-btn_2" onClick={previousCard} />
+            <input type="button" className={style.next} value="Next" id="Mnext-btn_2" onClick={nextCard} />
+          </Row>
+        </Row>
+      </section>
+
+      <section className={style.container3} id="containerCardM3">
+        <Row className={style.front3} id="front3">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Computer Training</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p>
+              Computer and Laptop Training is imparted at RSVI to the visually Impaired so that they can study and qualify various Competetive Examinations, qualify jobs and further impart training to other Visually Impaired students.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="Previous" id="Mprev-btn_3" onClick={previousCard} />
+            <input type="button" className={style.next} value="Next" id="Mnext-btn_3" onClick={nextCard} />
+          </Row>
+        </Row>
+      </section>
+
+      <section className={style.container4} id="containerCardM4">
+        <Row className={style.front4} id="front4">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Mobile Training</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p>
+              Mobile Training is imparted at RSVI to the visually Impaired so that they can study and communicate through smart phones , further impart training to other Visually Impaired students.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="Previous" id="Mprev-btn_4" onClick={previousCard} />
+            <input type="button" className={style.next} value="Next" id="Mnext-btn_4" onClick={nextCard} />
+          </Row>
+        </Row>
+      </section>
+
+      <section className={style.container5} id="containerCardM5">
+        <Row className={style.front5} id="front5">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Technical/Reading Without Seeing training</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p className={style.para5}>
+              Reading without seeing & Technological workshops are held at RSVI to aware Visually Impaired Persons about technology and new updations.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="Previous" id="Mprev-btn_5" onClick={previousCard} />
+            <input type="button" className={style.next} value="Next" id="Mnext-btn_5" onClick={nextCard} />
+          </Row>
+        </Row>
+      </section>
+
+      <section className={style.container6} id="containerCardM6">
+        <Row className={style.front6} id="front6">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Vocational Training</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p>
+              Vocational Training is imparted at RSVI to the visually Impaired so that they can study and qualify various Competetive Examinations, qualify jobs and further impart training to other Visually Impaired students.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="Previous" id="Mprev-btn_6" onClick={previousCard} />
+            <input type="button" className={style.next} value="Next" id="Mnext-btn_6" onClick={nextCard} />
+          </Row>
+        </Row>
+      </section>
+
+      <section className={style.container7} id="containerCardM7">
+        <Row className={style.front7} id="front7">
+          <Col className={style.imagePart}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX-jxeu2mRorUwNN3aOItkBaKJaoRgqBHV9Q&usqp=CAU" alt="Braille" />
+          </Col>
+          <Col className={style.heading}>
+            <h1>Misc workshops and Seminars</h1>
+            <div className={style.border}></div>
+          </Col>
+          <Col className={style.contentDetails}>
+            <p className={style.para7}>
+              Misc workshops and Seminars is imparted at RSVI to the visually Impaired so that they can aware Visually Impaired Persons about technology and new updations, latest technology and trends.
+            </p>
+          </Col>
+          <Row className={style.buttons}>
+            <input type="button" className={style.previous} value="Previous" id="Mprev-btn_7" onClick={previousCard} />
+            <input type="button" className={style.next} value="" />
+          </Row>
+        </Row>
+      </section>
+      </Container>
+    </div>
 )
 }
 
