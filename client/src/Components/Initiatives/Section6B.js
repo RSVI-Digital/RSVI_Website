4735554207a26e0096dcbadb95fb27caf3f8edc1
Anjali Chaturvedi <anjalichaturvedii@icloud.com>
2021-07-04 22:21:14 +0530

    section 6B updated

--- client/src/Components/Initiatives/Section6B.js
+++ client/src/Components/Initiatives/Section6B.js
@@ -6,89 +6,89 @@ const Section6B = () => {
 
   const nextCard = (e) => {
     if (e.target.id === 'next-btn_1') {
-      document.getElementById('containerCardM1').style.display = "none";
-      document.getElementById('containerCardM2').style.display = "block";
+      document.getElementById('containerCard6M1').style.display = "none";
+      document.getElementById('containerCard6M2').style.display = "block";
     }
     if (e.target.id === 'next-btn_2') {
-      document.getElementById('containerCardM2').style.display = "none";
-      document.getElementById('containerCardM3').style.display = "block";
+      document.getElementById('containerCard6M2').style.display = "none";
+      document.getElementById('containerCard6M3').style.display = "block";
     }
     if (e.target.id == 'next-btn_3') {
-      document.getElementById('containerCardM3').style.display = "none";
-      document.getElementById('containerCardM4').style.display = "block";
+      document.getElementById('containerCard6M3').style.display = "none";
+      document.getElementById('containerCard6M4').style.display = "block";
     }
     if (e.target.id == 'next-btn_4') {
-      document.getElementById('containerCardM4').style.display = "none";
-      document.getElementById('containerCardM5').style.display = "block";
+      document.getElementById('containerCard6M4').style.display = "none";
+      document.getElementById('containerCard6M5').style.display = "block";
     }
     if (e.target.id == 'next-btn_5') {
-      document.getElementById('containerCardM5').style.display = "none";
-      document.getElementById('containerCardM6').style.display = "block";
+      document.getElementById('containerCard6M5').style.display = "none";
+      document.getElementById('containerCard6M6').style.display = "block";
     }
     if (e.target.id == 'next-btn_6') {
-      document.getElementById('containerCardM6').style.display = "none";
-      document.getElementById('containerCardM7').style.display = "block";
+      document.getElementById('containerCard6M6').style.display = "none";
+      document.getElementById('containerCard6M7').style.display = "block";
     }
     if (e.target.id == 'next-btn_7') {
-      document.getElementById('containerCardM7').style.display = "none";
-      document.getElementById('containerCardM8').style.display = "block";
+      document.getElementById('containerCard6M7').style.display = "none";
+      document.getElementById('containerCard6M8').style.display = "block";
     }
     if (e.target.id == 'next-btn_8') {
-      document.getElementById('containerCardM8').style.display = "none";
-      document.getElementById('containerCardM9').style.display = "block";
+      document.getElementById('containerCard6M8').style.display = "none";
+      document.getElementById('containerCard6M9').style.display = "block";
     }
     if (e.target.id == 'next-btn_9') {
-      document.getElementById('containerCardM9').style.display = "none";
-      document.getElementById('containerCardM10').style.display = "block";
+      document.getElementById('containerCard6M9').style.display = "none";
+      document.getElementById('containerCard6M10').style.display = "block";
     }
     if (e.target.id == 'next-btn_10') {
-      document.getElementById('containerCardM10').style.display = "none";
-      document.getElementById('containerCardM1').style.display = "block";
+      document.getElementById('containerCard6M10').style.display = "none";
+      document.getElementById('containerCard6M1').style.display = "block";
     }
   }
 
   const previousCard = (e) => {
-    if (e.target.id == 'prev-btn_2') {
-      document.getElementById('containerCardM2').style.display = "none";
-      document.getElementById('containerCardM1').style.display = "block";
+    if (e.target.id == '6prev-btn_2') {
+      document.getElementById('containerCard6M2').style.display = "none";
+      document.getElementById('containerCard6M1').style.display = "block";
     }
-    if (e.target.id == 'prev-btn_3') {
-      document.getElementById('containerCardM3').style.display = "none";
-      document.getElementById('containerCardM2').style.display = "block";
+    if (e.target.id == '6prev-btn_3') {
+      document.getElementById('containerCard6M3').style.display = "none";
+      document.getElementById('containerCard6M2').style.display = "block";
     }
     if (e.target.id == 'prev-btn_4') {
-      document.getElementById('containerCardM4').style.display = "none";
-      document.getElementById('containerCardM3').style.display = "block";
+      document.getElementById('containerCard6M4').style.display = "none";
+      document.getElementById('containerCard6M3').style.display = "block";
     }
     if (e.target.id == 'prev-btn_5') {
-      document.getElementById('containerCardM5').style.display = "none";
-      document.getElementById('containerCardM4').style.display = "block";
+      document.getElementById('containerCard6M5').style.display = "none";
+      document.getElementById('containerCard6M4').style.display = "block";
     }
     if (e.target.id == 'prev-btn_6') {
-      document.getElementById('containerCardM6').style.display = "none";
-      document.getElementById('containerCardM5').style.display = "block";
+      document.getElementById('containerCard6M6').style.display = "none";
+      document.getElementById('containerCard6M5').style.display = "block";
     }
     if (e.target.id == 'prev-btn_7') {
-      document.getElementById('containerCardM7').style.display = "none";
-      document.getElementById('containerCardM6').style.display = "block";
+      document.getElementById('containerCard6M7').style.display = "none";
+      document.getElementById('containerCard6M6').style.display = "block";
     }
     if (e.target.id == 'prev-btn_8') {
-      document.getElementById('containerCardM8').style.display = "none";
-      document.getElementById('containerCardM7').style.display = "block";
+      document.getElementById('containerCard6M8').style.display = "none";
+      document.getElementById('containerCard6M7').style.display = "block";
     }
     if (e.target.id == 'prev-btn_9') {
-      document.getElementById('containerCardM9').style.display = "none";
-      document.getElementById('containerCardM8').style.display = "block";
+      document.getElementById('containerCard6M9').style.display = "none";
+      document.getElementById('containerCard6M8').style.display = "block";
     }
     if (e.target.id == 'prev-btn_10') {
-      document.getElementById('containerCardM10').style.display = "none";
-      document.getElementById('containerCardM9').style.display = "block";
+      document.getElementById('containerCard6M10').style.display = "none";
+      document.getElementById('containerCard6M9').style.display = "block";
     }
   }
 
   return (
     <Container className={style.mainContainer}>
-      <section className={style.mobilecontainer1} id="containerCardM1">
+      <section className={style.mobilecontainer1} id="containerCard6M1">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -108,7 +108,7 @@ const Section6B = () => {
         </Row>
       </section>
 
-      <section className={style.mobilecontainer2} id="containerCardM2">
+      <section className={style.mobilecontainer2} id="containerCard6M2">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -128,7 +128,7 @@ const Section6B = () => {
         </Row>
       </section>
 
-      <section className={style.mobilecontainer3} id="containerCardM3">
+      <section className={style.mobilecontainer3} id="containerCard6M3">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -147,7 +147,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer4} id="containerCardM4">
+      <section className={style.mobilecontainer4} id="containerCard6M4">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -166,7 +166,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer5} id="containerCardM5">
+      <section className={style.mobilecontainer5} id="containerCard6M5">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -185,7 +185,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer6} id="containerCardM6">
+      <section className={style.mobilecontainer6} id="containerCard6M6">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -204,7 +204,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer7} id="containerCardM7">
+      <section className={style.mobilecontainer7} id="containerCard6M7">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -223,7 +223,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer8} id="containerCardM8">
+      <section className={style.mobilecontainer8} id="containerCard6M8">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -242,7 +242,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer9} id="containerCardM9">
+      <section className={style.mobilecontainer9} id="containerCard6M9">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
@@ -261,7 +261,7 @@ const Section6B = () => {
           </Row>
         </Row>
       </section>
-      <section className={style.mobilecontainer10} id="containerCardM10">
+      <section className={style.mobilecontainer10} id="containerCard6M10">
         <Row>
           <Col className={style.mobileimage}>
             <img src="https://picsum.photos/seed/picsum/400/300" alt="dummy" />
