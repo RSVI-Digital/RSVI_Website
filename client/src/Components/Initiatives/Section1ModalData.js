9b79e502e7f4cab6a1fa163e94c6f207a14f4afe
ar30612 <ar30612@gmail.com>
2021-07-08 05:14:57 +0200

    Updates

---
+++ client/src/Components/Initiatives/Section1ModalData.js
@@ -0,0 +1,72 @@
+import Red from "../Home//Images/Backgrounds/red.png";
+import yellow from "../Home//Images/Backgrounds/yellow.png";
+import green from "../Home//Images/Backgrounds/green.png";
+import blue from "../Home//Images/Backgrounds/blue.png";
+
+const ModalData = [
+  {
+    id: "1",
+    imgbg: `${blue}`,
+    title: " Etext & EPUB",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1CYvXoWf4bYhHjPeW49XMYdBdQZZxQOcE",
+    content:
+      'E-text is a textual presentation of information and/or data made available in an electronic format to be used by a print disabled person. This text can be read and interacted with on a Computer, Smart Phone or Specially designed E-Book Players.\nEPUB\nEPUB, which stands for electronic publication, is an open file format. EPUB is based on Web Standards: it defines a means of representing, packaging and encoding structured and semantically enhanced Web content — including HTML, CSS, SVG, images, and other resources — for distribution in a single-file format. EPUB is an e-book file format that uses the "epub" file extension. The term is short for electronic publication and is sometimes styled ePub. EPUB is supported by many e-readers, and compatible software is available for most smartphones, tablets, and computers.\'',
+  },
+  {
+    id: "2",
+    imgbg: `${yellow}`,
+    title: "   Daisy ",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1CSlPO26p-jZ5DrlRF7XAyGQkzIqQq8C2",
+    content:
+      "DAISY STANDS FOR--Digital Access Information System.The system assists people who have challenges using regular printed media or even Braille.Recording in Daisy format is far more better than simple MP3 format.It includes navigation facilities, entering of bookmarks etc.It gives a feeling of holding a hard copy of the book in your hand.RSVI produces quality Audio Books on a large scale. RSVI is recognised for its quality Daisy Books both in terms of Pronunciation, recording quality and daisy parameters. RSVI produce books in Hindi, English, Urdu and Sanskrit Languages. RSVI has always had regular users from All over India and even aboard. RSVI records everything which a visually impaired person requires, be it a Book, a Guide, Question Banks, Hand Written Notes, Story books, Comics, Magazines, User Manuals etc.",
+  },
+  {
+    id: "3",
+    imgbg: `${green}`,
+    title: "Competition preparation ",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1y9Ap-1SFa7vp8pukJSVdOTlf4_TcECEo",
+    content:
+      "Educational & Competitive Classes are imparted at RSVI to the visually Impaired so that qualify various Competetive Examninations. ",
+  },
+  {
+    id: "4",
+    imgbg: `${Red}`,
+    title: "  Dedsevi",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1xoARq3pxpvZYxmmVwPpdkq3rjqn_bKXX",
+    content:
+      "RSVI runs a two year face to face diploma in Education Special Education for Visually Impaired under recognition from rehabilitation council of India. The Diploma Course in Special Education aims to develop professionals for special education within a broad framework of education in the present context. The course will enable pre-service teachers to acquire knowledge, develop competencies and practice skills to impart education to children with disabilities.This is a two-year face to face regular teacher training course. After intermediate any student could take admission in the course. ",
+  },
+  {
+    id: "5",
+    imgbg: `${blue}`,
+    title: "  Inclusive education",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1vP-hh6Ftdbqp4BlGtzqp2kX9nKi7cAo_",
+    content:
+      "RSVI aims and promotes Inclusive education. Inclusive education means different and diverse students learning side by side in the same classroom. They enjoy field trips and after-school activities together. They participate in student government together. And they attend the same sports meets and plays.",
+  },
+  {
+    id: "6",
+    imgbg: `${Red}`,
+    title: " Braille Production",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1CRKIevAvK8tUa5rGVxNMfbpPkLB9ahkn",
+    content:
+      "RSVI Braille Press takes the privilege to be the First Braille Press in the state of Uttar Pradesh. RSVI Produces Braille books. We have a huge Braille library as we are also an extension counter of NIEPVD, Dehradun. Till March 2019 RSVI has produced aprox 250 titles with 35000 copies and 6 lakh pages, including Gazal Books, Manuals, Course Books, Competition Books, University Books, Story Books, Invitation Cards, Question Papers, Braille marking stickers etc. RSVI is not only producing books for Uttar Pradesh but also for Other States like Madhya Pradesh, Bihar, Delhi including Unicef.RSVI received “Best Braille Press Award” from Viklangjan Vikas Vibhag, UP in 2018 on the Occasion of World Disabled Day.",
+  },
+  {
+    id: "7",
+    imgbg: `${yellow}`,
+    title: "   CRE Programams",
+    sideImg:
+      "https://drive.google.com/thumbnail?id=1uHa_De5PxFFOm9XDJNXaZXirceuUnlGO",
+    content:
+      "CRE (Continuing Rehabilitation Programme) Programmes on different topics and disabilities (Mainly designed for Special Educators & Trainers)\nSuch Awareness Workshops – to create awareness in people about disability",
+  },
+];
+
+export default ModalData;
