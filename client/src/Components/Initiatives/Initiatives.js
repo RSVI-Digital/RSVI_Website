70d5bf2bbed1fe38d3fcab7fad0058ea130dcd34
ar30612 <ar30612@gmail.com>
2021-07-09 21:30:05 +0200

    Final changes Initiatives file

--- client/src/Components/Initiatives/Initiatives.js
+++ client/src/Components/Initiatives/Initiatives.js
@@ -5,7 +5,7 @@ import Modules from "./Initiatives.module.css";
 import Section1 from './Section1';
 import Section2 from './Section2';
 import Section3 from './Section3';
-import Section4a from './Section4a';
+import Section4 from './Section4';
 import Section5 from './Section5';
 
 import Section6 from './Section6';
@@ -16,9 +16,9 @@ class Initiatives extends Component {
                 <Container className={Modules.containerInitiatives}>
                     <Section1></Section1>
                     <Section2></Section2>
-                    {/* <Section3></Section3> */}
-                    <Section4a></Section4a>
-                    {/* <Section5></Section5> */}
+                    <Section3></Section3>
+                    <Section4></Section4>
+                    <Section5></Section5> 
                     <Section6></Section6>
                 </Container>
             </div>
--- client/src/Components/Initiatives/Section4.js
+++ client/src/Components/Initiatives/Section4.js
@@ -1,50 +1,27 @@
 import React, { Component } from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
 import Image from './Image';
-import Style from './Section4.module.css';
+import {Row, Col} from 'react-bootstrap';
+import Modules from './Section4.module.css'
+
 class Section4 extends Component {
     render() {
         return (
-            <React.Fragment >
-
-     
-<h1 className={Style.top}>Reach/Recognition</h1>
-<div className={Style.containerfluid }>
-<div className={Style.abc}>
-
-<div className = {Style.blogpost}>
-    <div className = {Style.blogpostimg}>
-    <h1 className={Style.heading}> RSVI Madhya Pradesh</h1>
-        <img className={`${Style.Section4img}`} src={Image.Section4.img1} alt=""/>  
-    </div>
-   {/*"https://drive.google.com/uc?id=1BSUuUwe5fX0Be2Q_HMP_tVPFQqnkqLdI" */}
-    <div className = {Style.blogpostinfo}>
-        
-        
-        <p className={Style.blogposttext}>
-        RSVI has setup a branch in Unchehra to serve the blind in the
-                state of Madhya Pradesh, The Branch will empower the target
-                group by providing training, education, assistive devices and
-                accessible technology. Our objective is to reach the
-                ‘Unreached’. Unchehara, inspite of being a border town of Madhya
-                Pradesh, it has yet to enjoy the pleasures of infrastructure and
-                development. There is comparatively a higher number of persons
-                with disability in the area. Persons with visual impairment are
-                deprived of educational facilities. RSVI has decided to provide
-                educational, social and economical rehabilitation opportunities
-                to the target group.
-        </p>
-        {/* <a href ="#" class = "blogpostcta">read more</a> */}
-        </div>
-    </div>
-</div>
-</div>
-
- 
-
-
-
-    </React.Fragment>
+            <div className={Modules.mainReach}>
+                <h1 className={Modules.header}>Reach/Recognition</h1>
+                <Row className={Modules.reachRow}>
+                    <Col className={Modules.reachCol1}>
+                        <Row className={Modules.col1Row1}>
+                            <h3 className={Modules.heading1}>RSVI Madhya Pradesh</h3>
+                        </Row>
+                        <Row className={Modules.col1Row2}>
+                            <img src={Image.Section4} alt='Reach/Recognition' className={Modules.imageReach}></img>
+                        </Row>
+                    </Col>
+                    <Col className={Modules.reachCol2}>
+                        <p className={Modules.para}>RSVI has setup a branch in Unchehra to serve the blind in the state of Madhya Pradesh, The Branch will empower the target group by providing training, education, assistive devices and accessible technology. Our objective is to reach the ‘Unreached’. Unchehara, inspite of being a border town of Madhya Pradesh, it has yet to enjoy the pleasures of infrastructure and development. There is comparatively a higher number of persons with disability in the area. Persons with visual impairment are deprived of educational facilities. RSVI has decided to provide educational, social and economical rehabilitation opportunities to the target group.</p>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
