aa285a8d51b9e5c97afe4bb0cd82bd31c099a82f
shataxi <dubey.shataxi3010@gmail.com>
2021-07-13 19:46:41 +0530

    changes done

--- client/src/Components/AboutUs/Image.js
+++ client/src/Components/AboutUs/Image.js
@@ -1,32 +1,56 @@
 const Image = {
-  Section1: "https://drive.google.com/uc?id=1Ixb8UTLWhOKjPdEEH7cibfwElh8Gs9Jm",
-  Section3: "https://drive.google.com/uc?id=15PKwBt7AIZu0hYjmZECepeYqFTxerb9a",
-  Section5: "https://drive.google.com/uc?id=12zmq8aiALzMlEb1QrQuRmzNVXTMQZWc-",
+  Section1: "https://drive.google.com/uc?id=1tN6_isxho_rs0s1F2wXy9irl3unG_BzH",
+  Section3: "https://drive.google.com/uc?id=1e0WHPYqaTPK2l8nYrPCYg5zh-60721Y1",
+  Section5: "https://drive.google.com/uc?id=1vVsdRKWvq2-_t-O12gYURV-VF00VDi6M",
   Section6: {
-    Img1: "https://drive.google.com/uc?id=1N_ln_PzP5UiNeek84L6wLm7FrjIQ0FTj",
-    Img2: "https://drive.google.com/uc?id=1WOtI0n-74ygfYEdSHnYh0P85qGtGgBP5",
-    Img3: "https://drive.google.com/uc?id=10j0E88g-LtP9qxi4ClKZ8nsEPkCmK5xC",
+    Img1: "https://drive.google.com/uc?id=1jOnf4jqmDWnsc8ziVzI2mNvNRTdq7sHN",
+    Img2: "https://drive.google.com/uc?id=16Bhiw5L9Iy-VicJ2gkPdW49afe5ZoJz7",
+    Img3: "https://drive.google.com/uc?id=1eDLlVuY5yvzLM2CFwGhLoCjq__OGlwpy",
   },
   Section7: {
-    Img1: "https://drive.google.com/uc?id=1YKXDoO_4kMoCppsO-fw0z7QKm2O36mHn",
-    Img2: "https://drive.google.com/uc?id=1mBYHsiT4ikGKUzk2Hjz_ZKhvyYKh4dfC",
-    Img3: "https://drive.google.com/uc?id=1tc6DjdDpmBaKn7Tp1H9Ua_yxBJdukyw5",
-    Img4: "https://drive.google.com/uc?id=1ZtrHJIAJDrGBXUrVxKJDtQ1nU-mY7jxQ",
+    Img1: "https://drive.google.com/uc?id=14HuTffzx2HQeu0EtZcD0bbERCk68py9n",
+    Img2: "https://drive.google.com/uc?id=17ybF5Uw_VnhwYjm0mcQFbPzaRxl7IdQt",
+    Img3: "https://drive.google.com/uc?id=1ChtwFf5fhAT7vVDLRr3h7G2wmauFs8U1",
+    Img4: "https://drive.google.com/uc?id=1RwTHqQKPg2pQb4MHvWGc1bvCdoY7MMLY",
   },
   Section7b: {
-    Img1: "https://drive.google.com/uc?id=1w4u7Uj0_gItghRdkhDKTf6w9lA2podr9",
-    Img2: "https://drive.google.com/uc?id=1mfQSRTHKtai5jBLe7PMOBDHOauOBvdft",
-    Img3: "https://drive.google.com/uc?id=1ag96TlP0mLKRS6DWecQYJnzNzYRZvQgZ",
-    Img4: "https://drive.google.com/uc?id=10Y87f0sGCJ191JiXdR38q6l8wvQMcJCw",
+    Img1: "https://drive.google.com/uc?id=1wHdnPYmtJnDFI8toXwxk7OjCiuLUXWzP",
+    Img2: "https://drive.google.com/uc?id=1QTBfKOKHOWNj1YgQH7nSUPsgRUW25pbK",
+    Img3: "https://drive.google.com/uc?id=1V8emffBy0BzXEPozVqb-N9Pg-752ULxF",
+    Img4: "https://drive.google.com/uc?id=1EvoiZBQYoqiwMj7cHrRCbzHbkqvpENV8",
   },
-  Section8: "https://drive.google.com/uc?id=1vxaQVQ7nuH5GI_QR_1Fa_a8VL8nmQyF2",
+  Section8:
+    "https://drive.google.com/uc?id=16tb5cgq8uCsd96xBGGX7psfHJoqQltSk",
+  Section8nr:"https://drive.google.com/uc?id=1pUuxlNyYRnTL7McYIaAIlRsTMAJl_z-i",/*crop */
   Section9: {
-    Img1: "https://drive.google.com/uc?id=1hlRMlkkZ9ZhEoaZINQwONl6BP4_HV2d_",
-    Img2: "https://drive.google.com/uc?id=1cnxZo83kCJ9-OX8fC8ZSXibP15sTZeD4",
-    Img3: "https://drive.google.com/uc?id=1aaq-iGSROj2rMhwBpYto07UFyxL-_uph",
-    Img4: "https://drive.google.com/uc?id=1AiICk0iQBnk1_RfGBlU0ngf00uRyEedM",
-    Img5: "https://drive.google.com/uc?id=1RPk1xx5frMty-QM_ew3qSclJyeEpaeLy",
+    Img1: "https://drive.google.com/uc?id=1GaydxgE2L9fQpx5j-AIQDWzuJZaNGg2K",
+    Img2: "https://drive.google.com/uc?id=17Qaop_eaHfCmhAym_TN_-dPwWZhvWA_B",
+    Img3: "https://drive.google.com/uc?id=12JpU8AnoYRTl6AB7Mc1eFPZXW7zDwNpH",
+    Img4: "https://drive.google.com/uc?id=1cE87GQS8Q0FlN4vP91HyTJkLWVfWh-0e",
+    Img5: "https://drive.google.com/uc?id=1GVAdG6MMpX2LFcEc58AH7I2B0iU81k4U",
   },
 };
 
 export default Image;
+//old lnks
+// Section7: {
+//   Img1: "https://drive.google.com/uc?id=1YKXDoO_4kMoCppsO-fw0z7QKm2O36mHn",
+//   Img2: "https://drive.google.com/uc?id=1mBYHsiT4ikGKUzk2Hjz_ZKhvyYKh4dfC",
+//   Img3: "https://drive.google.com/uc?id=1tc6DjdDpmBaKn7Tp1H9Ua_yxBJdukyw5",
+//   Img4: "https://drive.google.com/uc?id=1ZtrHJIAJDrGBXUrVxKJDtQ1nU-mY7jxQ",
+// },
+// Section7b: {
+//   Img1: "https://drive.google.com/uc?id=1w4u7Uj0_gItghRdkhDKTf6w9lA2podr9",
+//   Img2: "https://drive.google.com/uc?id=1mfQSRTHKtai5jBLe7PMOBDHOauOBvdft",
+//   Img3: "https://drive.google.com/uc?id=1ag96TlP0mLKRS6DWecQYJnzNzYRZvQgZ",
+//   Img4: "https://drive.google.com/uc?id=10Y87f0sGCJ191JiXdR38q6l8wvQMcJCw",
+// },
+//section8 800x300 https://drive.google.com/uc?id=1Tx8Gayu5LvBQrRjfqzq13nipvafXF1M7
+//section8 900X300 https://drive.google.com/uc?id=1AOlh5_FGD7WKO4gexz2EETON42YEuROs
+//section8 600X200 https://drive.google.com/uc?id=1R01_7z5N3TeWmJ_nR72LNh6O4iTJVZ7-
+//section8 500X166 https://drive.google.com/uc?id=120oSYw-FFbCvucm8Pnt-AprcYeqs72hz
+//section8 400X133 https://drive.google.com/uc?id=1l1LusHhaQlguTB_C-o9aUmTkLSVQXRKh
+//Section8 : "https://drive.google.com/uc?id=12Yi4kZwO54S33cA7V4t1NHfmQ9v-Caa8", 200by100
+//Section8 :"https://drive.google.com/uc?id=1EhVkGdrCs_g7eAgkY8fQldZ3kCdAG3UE", 200 by 66
+//Section8 : "https://drive.google.com/uc?id=1GqX79zT33MR9n_F8Lm5Gxczv-OJW9iil", 100 by 50
+//Section8 :"https://drive.google.com/uc?id=1KHQ0BEfEfphuv4WhHZwZldTpxPbtSHYh", 100 by 33
