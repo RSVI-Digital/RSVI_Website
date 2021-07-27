810c17e1b41c07b2463f453c97b12ef9c4cb34ff
Vinayak1298 <vinayakmirani1298@gmail.com>
2021-07-28 00:42:50 +0530

    fianl roll

--- client/src/Components/Initiatives/Section4.js
+++ client/src/Components/Initiatives/Section4.js
@@ -30,11 +30,7 @@ class Section4 extends Component {
                 accessible technology. Our objective is to reach the
                 ‘Unreached’. Unchehara, inspite of being a border town of Madhya
                 Pradesh, it has yet to enjoy the pleasures of infrastructure and
-                development. There is comparatively a higher number of persons
-                with disability in the area. Persons with visual impairment are
-                deprived of educational facilities. RSVI has decided to provide
-                educational, social and economical rehabilitation opportunities
-                to the target group.
+                development.
               </p>
             </Col>
           </Row>
